3162421ebb53049591ce5a6e5003ab64fa25f256
Pavel Morozuk <morozuki@mail.ru>
2022-03-14 23:36:07 +0300

    new structure and added bubbles

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let randomLoc = Math.floor(Math.random() * 5);
+let location1 = randomLoc;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
 let guess;
 let hits = 0;
 let guesses = 0;
@@ -10,12 +11,12 @@ while (!isSunk) {
     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
-    } else {
+    } else {    
         guesses++;
-        if (guess == location1 || guess == location2 || guess == location3) {
+        if (guess === location1 || guess === location2 || guess === location3) {
             hits++;
             alert("HIT!");
-            if (hits == 3) {
+            if (hits === 3) {
                 isSunk = true;
                 alert("You sank my battleship!");
             }
