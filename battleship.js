580235ae94f72739e329725d834f3983916fe34b
Pavel Morozuk <morozuki@mail.ru>
2022-03-20 00:47:58 +0300

    Battleship update.

--- battleship.js
+++ battleship.js
@@ -1,29 +1,51 @@
-let randomLoc = Math.floor(Math.random() * 5);
-let location1 = randomLoc;
-let location2 = location1 + 1;
-let location3 = location2 + 1;
-let guess;
-let hits = 0;
-let guesses = 0;
-let isSunk = false;
-
-while (!isSunk) {
-    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
-    } else {    
-        guesses++;
-        if (guess === location1 || guess === location2 || guess === location3) {
-            hits++;
-            alert("HIT!");
-            if (hits === 3) {
-                isSunk = true;
-                alert("You sank my battleship!");
-            }
-        } else {
-          alert("MISS");
-        }
+let view = {
+    displayMessage: function (msg) {
+        let messArea = document.getElementById("messageArea");
+        messArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
     }
-}
-let status = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
-alert(status);
+};
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+            {locations: ["24", "34", "44"], hits: ["", "", ""]},
+            {locations: ["10", "11", "12"], hits: ["", "", ""]}],
+    fire: function (guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
