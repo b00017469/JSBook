f18c75753d94f70efcf13a091c22a29b2f5d122d
Pavel Morozuk <morozuki@mail.ru>
2022-03-20 22:45:39 +0300

    fixed battleship

--- battleship.js
+++ battleship.js
@@ -109,7 +109,7 @@ function parseGuess (guess) {
     if (guess === null || guess.length !== 2) {
         alert("Oops, please enter a letter and a number on the board.");
     } else {
-        let firstChar = guess.charAt(0);
+        let firstChar = guess.charAt(0).toUpperCase();
         let row = alfabet.indexOf(firstChar);
         let column = guess.charAt(1);
         if (isNaN(row) || isNaN(column)) {
